96e062ab6bee5bb1389076358e7bf6a111676860
gato365 <immanuelwilliams@gato365.com>
2023-05-06 18:38:33 -0700

    Made the model based on my specifications

---
+++ server/model/Stable.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const dailyLogSchema = new mongoose.Schema({
+  date: { type: Date },
+  wakeUp: { type: Date },
+  weightAM: { type: Number },
+  numberOfDays: { type: Number },
+  dayYear: { type: Number },
+  goals: { type: String, required: true },
+  foodQuality: { type: String, required: true },
+  mood: { type: String, required: true },
+  MJ: { type: Number },
+  alcohol: { type: Number },
+  eatingOut: { type: Number },
+  mode: { type: String },
+  myYFourLife: { type: String },
+  myYFourKelley: { type: String },
+  myYFourGATO365: { type: String },
+  blessings: { type: String },
+  notes: { type: String },
+  time: { type: String, required: true }
+});
+
+module.exports = mongoose.model('DailyLog', dailyLogSchema);
