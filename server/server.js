42f45cbd69e78d7960459ac61a2bc7378af56ef3
gato365 <immanuelwilliams@gato365.com>
2023-05-07 19:20:08 -0700

    Rearrange files in a folder

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const stableRoutes = require('./routes/stableRoutes');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,6 +13,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(stableRoutes);
+
+// Serve up static assets
+app.use(express.static(path.join(__dirname, '..', 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+// Default response for any other request (Not Found)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong');
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
